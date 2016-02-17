2debcfe3f221b2a65b654dbcca3a659d58c2e3f8
Jerry Verhoef <jerry@lucard.nl>
2016-02-17 15:48:05 +0100

    Configuration of the bundle can be done from the symfony config

---
+++ Factory/SymfonyCollection.php
@@ -0,0 +1,27 @@
+<?php
+namespace Qandidate\Bundle\ToggleBundle\Factory;
+
+use Qandidate\Toggle\Serializer\InMemoryCollectionSerializer;
+
+class SymfonyCollection
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return \Qandidate\Toggle\ToggleCollection\InMemoryCollection
+     */
+    public function getToggles()
+    {
+        $serializer = new InMemoryCollectionSerializer();
+        return $serializer->deserialize($this->config);
+    }
+}
