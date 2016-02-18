77d31146594d3dde48492db285291d95b8c4cb26
Jerry Verhoef <jerry@lucard.nl>
2016-02-18 09:53:57 +0100

    Cosmetic change added the DOC Block

--- Factory/SymfonyCollection.php
+++ Factory/SymfonyCollection.php
@@ -10,7 +10,10 @@ class SymfonyCollection
      */
     private $config;
 
-
+    /**
+     * SymfonyCollection constructor.
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
