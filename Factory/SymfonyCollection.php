838fd1a46f0f6344f0468fbfa1b4dd6f147daad5
Roel Philipsen <roel.philipsen@vnuvacaturemedia.nl>
2016-09-09 14:53:52 +0200

    Revert "Cosmetic change added the DOC Block"
    This reverts commit 77d31146594d3dde48492db285291d95b8c4cb26.

--- Factory/SymfonyCollection.php
+++ Factory/SymfonyCollection.php
@@ -10,10 +10,7 @@ class SymfonyCollection
      */
     private $config;
 
-    /**
-     * SymfonyCollection constructor.
-     * @param array $config
-     */
+
     public function __construct(array $config)
     {
         $this->config = $config;
